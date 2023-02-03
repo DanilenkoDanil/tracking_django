686ecc6270c8790237f7f572ee4a69cabada0498
Данил <danilenkodanil74@gmail.com>
2023-02-03 15:39:35 +0200

    calculate update

--- base/management/commands/bot.py
+++ base/management/commands/bot.py
@@ -44,7 +44,7 @@ async def process_start_command(message: types.Message, state: FSMContext):
     time = command_list[3]
     signals = SignalResult.objects.filter()
     for signal in signals:
-        balance += 0.25 * balance * ((100 * (signal.price_change * leverage)) / 100)
+        balance += (0.25 * balance) * ((100 + (signal.price_change * leverage)) / 100)
     print(command_list)
     await message.reply(f"Balance - {balance}!", parse_mode='Markdown')
 
